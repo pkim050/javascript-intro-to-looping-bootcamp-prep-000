c9c6346784707a505592200bdcfe599e74dd081b
Patrick A Kim <pkim050@gmail.com>
2018-11-08 19:43:06 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,37 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i = 1) {
+      array = "I am 1 strange loop.";
+    }
+    else {
+      array = "I am ${i} strange loops.";
+    }
+  }
+  return array;
+}
+
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(--number);
+  }
+  return "done";
+}
+/*
+var j = 0;
+function incrementVariable() {
+  j = j - 1;
+  if (j != 0) {
+    return true;
+  }
+  return false;
+}
+
+function doWhileLoop(array) {
+  do {
+    j = array.length;
+    incrementVariable();
+    array.pop();
+  } while (array.length > 0 && incrementVariable());
+  return array;
+}
+*/
