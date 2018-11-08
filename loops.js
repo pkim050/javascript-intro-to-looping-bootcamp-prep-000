0eb705f95184dde7f486c8db94f8a5184cc04438
Patrick A Kim <pkim050@gmail.com>
2018-11-08 20:29:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,12 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
-    if (i === 0) {
+    if (i !== 0) {
       console.log(array);
-      array = "I am 1 strange loop.";
+      array = "I am ${i} strange loops.";
       console.log(array);
     }
     else {
-      array = "I am ${i} strange loops.";
+      array = "I am 1 strange loop.";
     }
   }
   return array;
